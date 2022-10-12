c7207026ffba26a0e5610e761b61e1d70c425426
Mo Chahadeh <applednd2002@gmail.com>
2022-10-12 22:50:41 +0300

    added main loop

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+from settings import *
+
+while True: 
+
+    # Event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    # Game loop
+    WINDOW.fill(BGCOLOR)
+    pygame.display.update()
+    clock.tick(FPS)

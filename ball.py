6ec94d9b610667092e343caf4981bd0321347bee
Mo Chahadeh <applednd2002@gmail.com>
2022-10-13 00:31:16 +0300

    added ball motion settings and maths

--- ball.py
+++ ball.py
@@ -11,22 +11,35 @@ class Ball(pygame.sprite.Sprite):
         self.radius = 10
         self.pos = pygame.Vector2(WIDTH/2, HEIGHT/2)
         self.vel = pygame.Vector2(0, 0)
-        self.acc = pygame.Vector2(0, GRAVITY)
+        self.acc = pygame.Vector2(0, 0)
 
-        self.rect = pygame.rect.Rect(self.pos.x - self.radius, self.pos.y - self.radius, self.radius * 2, self.radius * 2)
+        self.rect:pygame.rect.Rect = pygame.rect.Rect(self.pos.x - self.radius, self.pos.y - self.radius, self.radius * 2, self.radius * 2)
     
     def draw(self):
-        # pygame.draw.rect(WINDOW, BALLCOLOR, pygame.Rect(self.rect.left, self.rect.top, self.rect.width, self.rect.height))
         pygame.gfxdraw.aacircle(WINDOW, int(self.pos.x), int(self.pos.y), self.radius, WHITECOLOR)
         pygame.gfxdraw.filled_circle(WINDOW, int(self.pos.x), int(self.pos.y), self.radius, WHITECOLOR)
 
     
     def update(self):
 
-        self.vel = self.acc
+        self.loop()
+
+        self.vel += self.acc
         self.pos += self.vel
-        self.pos.x -= self.pos.x * FRICTION
+        self.vel.x -= self.vel.x * FRICTION.x
+        self.vel.y -= self.vel.y * FRICTION.y
 
 
         self.rect = pygame.rect.Rect(self.pos.x - self.radius, self.pos.y - self.radius, self.radius * 2, self.radius * 2)
         self.draw()
+
+    def jump(self):
+        self.vel.y = -JUMPIMPLUSE
+    
+    def loop(self):
+
+        if self.rect.right < 0:
+            self.pos.x = WIDTH+self.radius
+        elif self.rect.left > WIDTH:
+            self.pos.x = -self.radius
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from matplotlib import backend_bases
 import pygame
 from settings import *
 from ball import *
@@ -11,6 +12,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                ball.jump()
+            if event.key == pygame.K_RIGHT:
+                ball.acc.x = 1
+            if event.key == pygame.K_LEFT:
+                ball.acc.x = -1
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ball.acc.x = 0
+            if event.key == pygame.K_LEFT:
+                ball.acc.x = 0
 
     # Game loop
     WINDOW.fill(BGCOLOR)
